9d5bcb5a95a3ac77a7f59812236470b24f8a09c7
Tomasz 'Krzaczek' Drewek <tomaszdrewek94@gmail.com>
2023-01-29 21:59:55 +0100

    initial commit

---
+++ DiscordCommon/Constants/Emojis.cs
@@ -0,0 +1,26 @@
+﻿using Discord;
+
+namespace DiscordCommon.Constants
+{
+    public static class Emojis
+    {
+        public readonly static Emoji ArrowForward = new Emoji("\u25B6\uFE0F");
+        public readonly static Emoji Cry = new Emoji("\uD83D\uDE22");
+        public readonly static Emoji LoudSound = new Emoji("\uD83D\uDD0A");
+        public readonly static Emoji MiddleFinger = new Emoji("\uD83D\uDD95");
+        public readonly static Emoji Mute = new Emoji("\uD83D\uDD07");
+        public readonly static Emoji PlayPause = new Emoji("\u23EF\uFE0F");
+        public readonly static Emoji Punch = new Emoji("\uD83D\uDC4A");
+        public readonly static Emoji Rofl = new Emoji("\uD83E\uDD23");
+        public readonly static Emoji Scream = new Emoji("\uD83D\uDE31");
+        public readonly static Emoji Sound = new Emoji("\uD83D\uDD09");
+        public readonly static Emoji Speaker = new Emoji("\uD83D\uDD08");
+        public readonly static Emoji Sunglasses = new Emoji("\uD83D\uDE0E");
+        public readonly static Emoji SweatSmile = new Emoji("\uD83D\uDE05");
+        public readonly static Emoji ThumbsDown = new Emoji("\uD83D\uDC4E");
+        public readonly static Emoji ThumbsUp = new Emoji("\uD83D\uDC4D");
+        public readonly static Emoji TrackNext = new Emoji("\u23ED\uFE0F");
+        public readonly static Emoji TrackPrevious = new Emoji("\u23EE\uFE0F");
+        public readonly static Emoji X = new Emoji("\u274C");
+    }
+}
